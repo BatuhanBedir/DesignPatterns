c7eabb8a2b665b7ca43a4a5b00d387e4ecaa96b0
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-07-23 21:39:00 +0300

    iterator

---
+++ DesignPatterns/Iterator/IteratorPattern/VisitRoute.cs
@@ -0,0 +1,8 @@
+﻿namespace Iterator.IteratorPattern;
+
+public class VisitRoute
+{
+    public string CountryName { get; set; }
+    public string CityName { get; set; }
+    public string VisitPlaceName { get; set; }
+}
